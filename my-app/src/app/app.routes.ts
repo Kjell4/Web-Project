3cd60cefbf83d9d2386deb4d014b9dbf0437664e
Kjell4 <ilias.bekturgan@mail.ru>
2024-03-30 21:03:55 +0600

    Routing setting, UI edited

--- my-app/src/app/app.routes.ts
+++ my-app/src/app/app.routes.ts
@@ -1,4 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { CategoryComponent } from './category/category.component';
 
 export const routes: Routes = [
+    {path: '', redirectTo:'home', pathMatch:'full'},
+    {path: 'home', component: HomeComponent, title: 'Home'},
+    {path: 'category', component: CategoryComponent, title: 'Category'},
+    {path: 'about', component: AboutComponent, title: 'About'}
 ];
